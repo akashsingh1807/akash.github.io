7aaeb473039dff654fd37c384bca55fb0537a0a7
Akashsingh01 <engg.akashsingh@gmail.com>
2025-03-24 15:31:31 +0530

    updated details

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -30,16 +30,16 @@ const skillCategories = [
 ];
 
 const certifications = [
-    { title: 'Career Essentials in GitHub Professional Certificate', issuer: 'GitHub', date: 'Mar 2025', link: '#' },
-    { title: 'Java Foundations Professional Certificate', issuer: 'JetBrains', date: 'Mar 2025', link: '#' },
-    { title: 'Microservices Foundations Professional Certificate', issuer: 'Kong Inc.', date: 'Mar 2025', link: '#' },
-    { title: 'AWS Essential Training for Developers', issuer: 'LinkedIn', date: 'Apr 2024', link: '#' },
+    { title: 'Career Essentials in GitHub Professional Certificate', issuer: 'GitHub', date: 'Mar 2025', link: 'https://www.linkedin.com/learning/certificates/3c78c1eea024943b228aeb8887f215bc18f578270515ad726649c4a3fc0ce778?u=2205354' },
+    { title: 'Java Foundations Professional Certificate', issuer: 'JetBrains', date: 'Mar 2025', link: 'https://www.linkedin.com/learning/certificates/a21dd82d50c3c5f1d0889e4f09c6e1a6ae3b55d9a1a27f007571bec1ebb59763?u=2205354' },
+    { title: 'Microservices Foundations Professional Certificate', issuer: 'Kong Inc.', date: 'Mar 2025', link: 'https://www.linkedin.com/learning/certificates/06357b41a1b60e06be3423266febf591a912473f4a71b2898a0849759b8bf13f?u=2205354' },
+    { title: 'AWS Essential Training for Developers', issuer: 'LinkedIn', date: 'Apr 2024', link: 'https://www.linkedin.com/learning/certificates/604b96285138356302982aae187e1f7d98223f40ebaf9277885691d57c736540?u=2205354' },
     { title: 'Spring & Hibernate for Beginners (Spring Boot)', issuer: 'Udemy', date: 'May 2021', link: 'https://www.udemy.com/certificate/UC-4d7db98c-5b13-4d81-b848-51c1832c9dba/' },
     { title: 'Secure Coding - Secure Application Development', issuer: 'Udemy', date: 'Nov 2020', link: 'https://www.udemy.com/certificate/UC-56494948-057f-4853-b09b-be0106b5134e/' },
     { title: 'Design Patterns in Java', issuer: 'Udemy', date: 'Aug 2020', link: 'https://www.udemy.com/certificate/UC-1906fc8a-d4e1-4c4e-b2c7-dd4edda5b1f4/' },
-    { title: 'Proficiency in English', issuer: 'Aspiring Minds', date: 'Jun 2019', link: '#' },
-    { title: 'Software Development Trainee', issuer: 'Aspiring Minds', date: 'Jun 2019', link: '#' },
-    { title: 'JavaScript Certification', issuer: 'Sololearn', date: 'May 2019', link: '#' },
+    { title: 'Proficiency in English', issuer: 'Aspiring Minds', date: 'Jun 2019', link: 'https://www.myamcat.com/certificate/9539239/proficiency-in-english/284' },
+    { title: 'Software Development Trainee', issuer: 'Aspiring Minds', date: 'Jun 2019', link: 'https://www.myamcat.com/certificate/9539239/software-development-trainee/166' },
+    { title: 'JavaScript Certification', issuer: 'Sololearn', date: 'May 2019', link: 'https://www.sololearn.com/Certificate/CT-5RQPANSV/pdf' },
 ];
 
 const SkillCard = ({ category, skills }: { category: string; skills: string[] }) => {
