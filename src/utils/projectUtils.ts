555ed511f0e237b7e9433372e97aa9492a4ebf69
Akashsingh01 <engg.akashsingh@gmail.com>
2025-04-15 16:00:50 +0530

    imaages path added

--- src/utils/projectUtils.ts
+++ src/utils/projectUtils.ts
@@ -13,14 +13,14 @@ export interface ProjectType {
   attribution?: string;
 }
 
-// Projects data taken from Saurabh Rai's portfolio with permission
+
 export const projectsData: ProjectType[] = [
   {
     id: 1,
     title: 'NextGen Healthcare ',
     description:
-        '\Working as a Senior Software Engineer in the healthcare domain, contributing to the development and enhancement of healthcare solutions. Involved in designing and developing scalable, high-performance applications using Java, Spring Boot, and Microservices architecture. Responsible for building and optimizing RESTful APIs, ensuring seamless integration with healthcare data systems, and implementing cloud-based solutions for improved efficiency.',
-    image: '/images/NexGenPatientPortal.webp',
+        'Working as a Senior Software Engineer in the healthcare domain, contributing to the development and enhancement of healthcare solutions. Involved in designing and developing scalable, high-performance applications using Java, Spring Boot, and Microservices architecture. Responsible for building and optimizing RESTful APIs, ensuring seamless integration with healthcare data systems, and implementing cloud-based solutions for improved efficiency.',
+    image: 'images/NexGenPatientPortal.webp',
     tags: [
       'Java', 'Spring Boot', 'Microservices', 'REST API', 'AWS',
       'Azure', 'Hibernate', 'MySQL', 'PostgreSQL', 'Docker',
@@ -35,7 +35,7 @@ export const projectsData: ProjectType[] = [
     title: 'Network Automation - OpenText',
     description:
         'Working as a Senior Software Engineer in the development and containerization of products for compliance, monitoring, and provisioning of large-scale network devices. Designed and implemented high-performance REST APIs, migrated the codebase from Java 8 to Java 11, and automated test cases to enhance efficiency. Contributed to the scalability and deployment of microservices using Docker and cloud platforms like AWS and Azure.',
-    image: '/images/network_automation.jpg',
+    image: 'images/network_automation.jpg',
     tags: [
       'Java', 'Spring Framework', 'Spring Boot', 'Microservices', 'REST API',
       'Docker', 'Kubernetes', 'AWS', 'Azure', 'MySQL',
@@ -49,7 +49,7 @@ export const projectsData: ProjectType[] = [
     title: 'Frontier - Business Licensing Platform (Singapore Government)',
     description:
         'Contributed to the development of Frontier, a one-stop Business Licensing platform for Singapore Community end-users, under the Singapore Government. Worked on implementing various licenses, developing and maintaining RESTful APIs, and handling production defect resolution. Involved in backend development using Spring MVC, Java, Hibernate, and MySQL, and frontend development using JSP, JavaScript, jQuery, HTML, and CSS. Ensured seamless integration between modules and optimized system performance.',
-    image: '/images/frontier.png',
+    image: 'images/frontier.png',
     tags: [
       'Spring MVC', 'Java', 'Hibernate', 'MySQL', 'RESTful API',
       'JSP', 'JavaScript', 'jQuery', 'HTML', 'CSS',
